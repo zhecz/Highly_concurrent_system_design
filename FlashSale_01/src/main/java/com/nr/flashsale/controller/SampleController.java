6f4bee8aac454edc350c9b65c291afa744a5c5ed
zhe <youcanfindivy@gmail.com>
2022-12-08 15:42:13 -0500

    add flash_sale 01

---
+++ FlashSale_01/src/main/java/com/nr/flashsale/controller/SampleController.java
@@ -0,0 +1,34 @@
+package com.nr.flashsale.controller;
+
+import com.nr.flashsale.redis.RedisService;
+import com.nr.flashsale.result.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/demo")
+public class SampleController {
+
+    @Autowired
+    RedisService redisService;
+
+
+    @RequestMapping("/redis/get")
+    @ResponseBody
+    public Result<Long> redisGet() {
+        Long v1 = redisService.get("key1", Long.class);
+        return Result.success(v1);
+
+    }
+
+    @RequestMapping("/redis/set")
+    @ResponseBody
+    public Result<String> redisSet() {
+        boolean ret = redisService.set("key2", "hello");
+        String str = redisService.get("key2", String.class);
+        return Result.success(str);
+    }
+
+}
